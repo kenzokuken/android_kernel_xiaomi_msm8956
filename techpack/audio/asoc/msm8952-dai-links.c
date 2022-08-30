fcf51724171c48bbb1ae977ee914be26e53a8978
zenosa <benarji7677@gmail.com>
2022-08-30 14:45:34 +0800

    techpack: ASoC: Add the tasha driver for MSM8956
    Add the driver to enable audio support for external codec on MSM8956.
    
    Signed-off-by: Benarji Anand <benarji385@gmail.com>

--- techpack/audio/asoc/msm8952-dai-links.c
+++ techpack/audio/asoc/msm8952-dai-links.c
@@ -24,6 +24,7 @@ enum {
 enum TASHA_LITE_DEVICE {
 	MSM8952_TASHA_LITE = 0,
 	MSM8953_TASHA_LITE,
+	MSM8956_TASHA_LITE,
 	NUM_OF_TASHA_LITE_DEVICE,
 };
 
@@ -1597,7 +1598,8 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 	enum codec_variant codec_ver = 0;
 	const char *tasha_lite[NUM_OF_TASHA_LITE_DEVICE] = {
 		"msm8952-tashalite-snd-card",
-		"msm8953-tashalite-snd-card"
+		"msm8953-tashalite-snd-card",
+		"msm8956-tashalite-snd-card"
 	};
 
 	card->dev = dev;
@@ -1616,6 +1618,8 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 				card->name = tasha_lite[MSM8952_TASHA_LITE];
 			else if (!strcmp(card->name, "msm8953-tasha-snd-card"))
 				card->name = tasha_lite[MSM8953_TASHA_LITE];
+			else if (!strcmp(card->name, "msm8956-tasha-snd-card"))
+				card->name = tasha_lite[MSM8956_TASHA_LITE];
 		}
 
 		len1 = ARRAY_SIZE(msm8952_common_fe_dai);
