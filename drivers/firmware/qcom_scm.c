debacf4d13d87d9948639d6df089d989dc4335e1
Benarji Anand <benarji385@gmail.com>
2024-07-06 14:56:51 +0800

    drivers: qcom: Add MSM8976 crypto interface
    MSM8976 includes an interface to communicate with
    the secure firmware that allows clients to request
    CPU power on/off, HDCP requests, loading firmware
    and other related actions.
    
    Signed-off-by: Benarji Anand <benarji385@gmail.com>

--- drivers/firmware/qcom_scm.c
+++ drivers/firmware/qcom_scm.c
@@ -600,6 +600,11 @@ static const struct of_device_id qcom_scm_dt_match[] = {
 	{ .compatible = "qcom,scm-msm8960",
 	  .data = (void *) SCM_HAS_CORE_CLK,
 	},
+	{ .compatible = "qcom,scm-msm8976",
+	  .data = (void *)(SCM_HAS_CORE_CLK
+			   | SCM_HAS_IFACE_CLK
+			   | SCM_HAS_BUS_CLK),
+	},
 	{ .compatible = "qcom,scm-msm8996",
 	  .data = NULL, /* no clocks */
 	},
